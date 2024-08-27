918fd3da29128a436078fd17d52bf537ef0395c7
w. Patrick Gale <w.patrick.gale@unc.edu>
2024-08-27 19:07:51 -0400

    package test

---
+++ v5.13/DvApiMod-pip-package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from DvApiMod-pip-package import __version__
+
+setup(
+    name='DvApiMod-pip-package',
+    version=__version__,
+    url='https://github.com/kuhlaid/dv-api-test',
+    author='w. Patrick Gale',
+    author_email='w.patrick.gale@gmail.com',
+    py_modules=['DvApiMod-pip-package'],
+)
